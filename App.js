2802b21a0b32e2bb23730ad68094d5b526576fcb
Bruno <bruno1.996@outlook.com>
2024-11-24 16:50:06 -0300

    Feito as telas

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
+import './gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { HomeScreen } from './components/HomeScreen';
-import { DetailsScreen } from './components/DetailsScreen';
-import { MoviesScreen } from './components/MoviesScreen';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import HomeScreen from './components/HomeScreen';
+import PesquisaAluno from './components/PesquisaAluno';
+import FinanceiroAluno from './components/FinanceiroAluno';
+import DetalhesAluno from './components/DetalhesAluno';
+import Agenda from './components/Agenda';
 
-const Stack = createNativeStackNavigator();
+import { PaperProvider } from 'react-native-paper';
+
+const Drawer = createDrawerNavigator();
+
+const theme = {
+  dark: false,
+  colors: {
+    primary: '#A9C9E0',
+    secondary: '#0056b3',
+    tertiary: '#2c2f22',
+    background: 'white',
+    card: 'white',
+    text: 'black',
+    border: 'black',
+    notification: 'black',
+  },
+};
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen}/>
-        <Stack.Screen name="Details" component={DetailsScreen}/>
-        <Stack.Screen name="Movies" component={MoviesScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <PaperProvider theme={theme}>
+        <NavigationContainer>
+          <Drawer.Navigator>
+            <Drawer.Screen name="Home" component={HomeScreen}/>
+            <Drawer.Screen name="PesquisaAluno" options={{
+              title: 'Pesquisa de Aluno',
+            }} component={PesquisaAluno}/>
+            <Drawer.Screen name="FinanceiroAluno" options={{
+              title: 'Financeiro',
+            }} component={FinanceiroAluno}/>
+            <Drawer.Screen name="Agenda" component={Agenda}/>
+            <Drawer.Screen name="DetalhesAluno" options={{
+              title: 'Detalhes do Aluno',
+            }} component={DetalhesAluno}/>
+          </Drawer.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
   );
 }
-
 
